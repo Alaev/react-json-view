ad3e35bc5a05dc048c4902d8ceb05a1f5f72f050
Michael Alaev <alaev.michael@gmail.com>
2020-12-09 22:52:42 +0200

    Added Icons

---
+++ src/components/icons/chevron-right.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function ChevronRight() {
+  return (
+    <span className="icon">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M9 5l7 7-7 7"
+        />
+      </svg>
+    </span>
+  );
+}
+
+export default ChevronRight;
